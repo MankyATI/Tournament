d7fe37bde58b8f3304674c5268bc898ab0037b9d
Mayank Pahuja <consultant-mayank.pahuja@mcb.mu>
2021-02-26 18:59:28 +0530

    added pods and gitignore for them

--- Tournament/Pods/Target Support Files/Pods-Tournament-TournamentUITests/Pods-Tournament-TournamentUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tournament_TournamentUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Tournament_TournamentUITests
-@end
--- Tournament/Pods/Target Support Files/Pods-Tournament/Pods-Tournament-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tournament : NSObject
-@end
-@implementation PodsDummy_Pods_Tournament
-@end
